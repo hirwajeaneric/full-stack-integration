ddf73f456b8e5f8aee714968bbdbe7446a85b06b
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-01-18 10:43:54 +0200

    Setup (backend) + Controllers  + Routes

---
+++ server/README.md
@@ -0,0 +1,2 @@
+# online_cement_delivery_app_server
+This is an online cement ordering and delivery application that allows a user to order any amount of cement from anywhere they want in Rwanda. Keep track of the order and delivery process of the products, and get the ordered cement to their door steps.
---
+++ server/controllers/contact.controller.js
@@ -0,0 +1,69 @@
+const { StatusCodes } = require('http-status-codes');
+const ContactModel = require('../models/contact');
+
+const add = async (req, res, next) => {
+    try {
+        const { fullName, email, phone } = req.body; 
+        const newContact = new ContactModel(fullName, email, phone);
+        var savedContact = await newContact.save();
+        res.status(StatusCodes.CREATED).json({ message: 'Contact successfully added!', contact: savedContact });
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+const updateContact = async (req, res, next) => {
+    try {
+        const id = req.query.id;
+        const updatedContact = await ContactModel.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(StatusCodes.OK).json({ message: 'Contact updated!', contact: updatedContact});
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+const list = async (req, res, next) => {
+    try {
+        const listOfContacts = await ContactModel.find({});
+        res.status(StatusCodes.OK).json({ contacts: listOfContacts });
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+const findById = async (req, res, next) => {
+    try {
+        const id = req.query.id;
+        const foundContact = await ContactModel.findById(id);
+        res.status(StatusCodes.OK).json({ contact: foundContact })
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+const findByPhone = async (req, res, next) => {
+    try {
+        const phone = req.query.phone;
+        const foundContact = await ContactModel.find({ phone: phone });
+        res.status(StatusCodes.OK).json({ contact: foundContact })
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+const deleteContact = async (req, res, next) => {
+    try {
+        const id = req.query.id;
+        const deletedContact = await ContactModel.findByIdAndDelete(id);
+        if (deletedContact) {
+            res.status(StatusCodes.OK).json({ message: 'Contact deleted' });
+        }
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error });
+    }
+};
+
+
+module.export = {
+    add, list, findById, findByPhone, updateContact, deleteContact
+}
