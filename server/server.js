97bb4372a017602306dcb463d110735d237436af
HIRWA Jean Eric <hirwajeaneric25@gmail.com>
2024-01-21 20:19:12 +0200

    Update server.js => Refactoring

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 var corsOptions = {
-    origin: ['http://localhost:5173', 'https://192.168.196.82:5273', 'http://localhost:5173', 'https://127.0.0.1:5273'],
+    origin: ['http://localhost:5173', 'http://192.168.196.82:5273', 'http://localhost:5173', 'http://127.0.0.1:5273'],
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization'
 }
